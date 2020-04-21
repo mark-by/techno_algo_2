3c181019f69dd1b251dc2255741a68de605f4257
Mark Bykhovets <mb@hrsmnt.ru>
2020-04-22 02:03:11 +0300

    Контест пройден

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 template<class T>
 struct DefaultComparator {
@@ -10,6 +11,13 @@ struct DefaultComparator {
 
 template<class Key, class Comparator = DefaultComparator<Key>>
 class Tree {
+    enum NodeType {
+        leftDone, rootWas, rightDone
+    };
+    enum OrderType {
+        inOrderType, postOrderType, preOrderType
+    };
+
     struct Node {
         Key key;
         Node *left;
@@ -21,20 +29,47 @@ class Tree {
     };
 
 public:
-    Tree() : root(nullprt) {};
+    Tree() : root(nullptr) {};
+    ~Tree() {
+        _order(passNode, delNode, postOrderType);
+    }
 
     void add(Key &key);
 
-    void inOrderPrint();
+    template<class Action>
+    void inOrder(Action action);
+
+    template<class Action>
+    void postOrder(Action action);
+
+    template<class Action>
+    void preOrder(Action action);
 
 private:
     Node *root;
     Comparator comp;
+
+    static Key getKeyNode(Node *node) {
+        return node->key;
+    }
+
+    static void delNode(Node * node) {
+        delete node;
+    }
+
+    static Node * passNode(Node * node) {
+        return node;
+    }
+
+    template<class NodeAction, class Action>
+    void _order(NodeAction nodeAction, Action action, std::uint8_t orderType);
 };
 
 void test(Tree<long> &tree);
 
 int main() {
+    Tree<long> tree;
+    test(tree);
     return 0;
 }
 
@@ -42,6 +77,7 @@ template<class Key, class Comparator>
 void Tree<Key, Comparator>::add(Key &key) {
     if (!root) {
         root = new Node(key);
+        return;
     }
     Node *curr = root;
     while (curr) {
@@ -64,8 +100,69 @@ void Tree<Key, Comparator>::add(Key &key) {
 }
 
 template<class Key, class Comparator>
-void Tree<Key, Comparator>::inOrderPrint() {
+template<class NodeAction, class Action>
+void Tree<Key, Comparator>::_order(NodeAction nodeAction, Action action, std::uint8_t orderType) {
+    std::stack<Node *> history;
+    std::stack<std::uint8_t> description;
+    history.push(root);
+    description.push(rootWas);
+    while (!history.empty()) {
+        switch (description.top()) {
+            case rootWas:
+                if (orderType == preOrderType) { action(nodeAction(history.top())); }
+                if (history.top()->left) {
+                    history.push(history.top()->left);
+                    description.push(rootWas);
+                } else {
+                    description.top() = leftDone;
+                }
+                break;
+            case leftDone:
+                if (orderType == inOrderType) { action(nodeAction(history.top())); }
+                if (history.top()->right) {
+                    history.push(history.top()->right);
+                    description.push(rootWas);
+                } else {
+                    description.top() = rightDone;
+                }
+                break;
+            case rightDone:
+                if (orderType == postOrderType) { action(nodeAction(history.top())); }
+                history.pop();
+                description.pop();
+                if (description.empty()) {
+                    break;
+                }
+                if (description.top() == rootWas) {
+                    description.top() = leftDone;
+                } else {
+                    description.top() = rightDone;
+                }
+                break;
+        }
+    }
+}
 
+template<class Key, class Comparator>
+template<class Action>
+void Tree<Key, Comparator>::postOrder(Action action) {
+    _order(getKeyNode, action, postOrderType);
+}
+
+template<class Key, class Comparator>
+template<class Action>
+void Tree<Key, Comparator>::inOrder(Action action) {
+    _order(getKeyNode, action, inOrderType);
+}
+
+template<class Key, class Comparator>
+template<class Action>
+void Tree<Key, Comparator>::preOrder(Action action) {
+    _order(getKeyNode, action, preOrderType);
+}
+
+void print(long key) {
+    std::cout << key << " ";
 }
 
 void test(Tree<long> &tree) {
@@ -78,5 +175,5 @@ void test(Tree<long> &tree) {
         tree.add(number);
         idx++;
     }
-    tree.inOrderPrint();
+    tree.inOrder(print);
 }
